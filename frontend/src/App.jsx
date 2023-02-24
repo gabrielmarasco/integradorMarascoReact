063afcdb99042f09e2217d18306b3f60958d7b0b
gabrielmarasco <gamarasco4@gmail.com>
2023-02-24 16:24:22 -0300

    product in app test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { Route, Routes, Link } from 'react-router-dom';
+import '../public/css/index.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Header />} />
-      </Routes>
+      <header className="App-header">
+        <Link to={'/'}>
+          <h2>CONCESIONARIA</h2>
+        </Link>
+      </header>
+      <main>
+        <h1>List products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.img} alt={product.model} />
+            <h3>
+              {product.marc} {product.model}
+            </h3>
+            <p>{product.description}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
